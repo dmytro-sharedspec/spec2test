ac6f05979628946925f52ca64ac10bba17cb3df7
dmytro <dmytro@sharedspec.com>
2025-07-12 19:57:06 +0200

    testing

--- feature2junit/src/main/java/dev/spec2test/feature2junit/FeatureFilePath.java
+++ feature2junit/src/main/java/dev/spec2test/feature2junit/FeatureFilePath.java
@@ -8,6 +8,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FeatureFilePath {
+
     /**
      * @return  the path to the feature file based on which the test was generated.
      */
